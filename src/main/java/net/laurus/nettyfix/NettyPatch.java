15bda18c6eed898f8452cfe2cb3006b7b7ad4298
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-10 14:10:01 +0100

    Migrate to unified build script

--- src/main/java/net/laurus/nettyfix/NettyPatch.java
+++ src/main/java/net/laurus/nettyfix/NettyPatch.java
@@ -13,7 +13,7 @@ public class NettyPatch {
 	
 	public static final String MODID = "NettyPatch";
 	public static final String NAME = "NettyPatch";
-	public static final String VERSION = "0.2";
+	public static final String VERSION = "GRADLETOKEN_VERSION";
 	
 	@Instance(MODID)
 	public static NettyPatch instance;	
